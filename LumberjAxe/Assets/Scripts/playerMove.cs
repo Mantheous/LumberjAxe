4976b0cd820b531f2f7840fbcd0ce50e2b89dc14
MrBiscuit15 <bounceybiscuit@gotmail.com>
2021-01-09 16:01:37 -0700

    Basic Character Controller

---
+++ LumberjAxe/Assets/Scripts/playerMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+    public float speed = 10f;
+    public float jumpheight = 10f;
+
+    [SerializeField] private LayerMask platformLayerMask;
+
+    private Rigidbody2D player;
+    private CapsuleCollider2D playerCollider;
+    private Vector2 moveVelocity;
+
+    void Update()
+    {
+        player = GetComponent<Rigidbody2D>();
+        playerCollider = GetComponent<CapsuleCollider2D>();
+
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), 0);
+        moveVelocity = moveInput.normalized * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        player.MovePosition(player.position + moveVelocity * Time.fixedDeltaTime);
+
+        if (IsGrounded() && Input.GetButtonDown("Jump"))
+        {
+            player.AddForce(transform.up * jumpheight);
+        }
+    }
+    private bool IsGrounded()
+    {
+        Physics2D.Raycast(playerCollider.bounds.center, Vector2.down, playerCollider.bounds.extents.y, + 1);
+        return true;
+    }
+}
