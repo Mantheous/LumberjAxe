a3dc14a722f8b286bcf6114930847dc2572d9f76
Mantheous <Mantheous@gmail.com>
2021-01-09 16:07:26 -0700

    Section1

---
+++ LumberjAxe/Assets/GroundCheck.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    public JackMove move;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        move.GroundSet(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        move.GroundSet(false);
+    }
+}
